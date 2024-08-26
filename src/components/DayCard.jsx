596621ce0b0db8cd77c14dbb913c7788e24ff8a3
DiiegoAG <diego@apdevs.com>
2024-08-25 19:17:19 -0600

    Current day is colored

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -2,13 +2,17 @@ import moment from 'moment';
 moment.locale('en')
 
 export function DayCard ({index, day, month, year, setAppointment}) {
+    const currentFormat = 'YYYY-MM-DD'
+    const now = moment().format(currentFormat)
+    const dateFromDay = moment(`${year}-${month + 1}-${day}`, currentFormat).format(currentFormat)
+
     const handleClick = () => {
         const dateFormat = 'YYYY-MM-DDTHH:mm:ss'
         let name = window.prompt("Name of event")
         let time = window.prompt("Time in 24 hour format", "13:00")
 
     return (
-        <div key={index} className={`day ${day ? '' : 'empty'}`} onClick={handleClick}>
+        <div key={index} className={`day${day ? '' : 'empty'}${now == dateFromDay ? ' current-day' : '' }`} onDoubleClick={handleClick}>
             <a>{day}</a>
         </div>
     )
